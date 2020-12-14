a529c6eff122ff4d43abe0cb0136f14ef669d9ad
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-12-14 22:02:03 +0000

    Add methods to menu object.

--- script.js
+++ script.js
@@ -36,6 +36,35 @@ const menu = {
       desserts: this.desserts,
     };
   },
+
+  addDishToCourse(courseName, dishName, dishPrice) {
+    const dish = { dishName, dishPrice };
+    return this._courses[courseName].push(dish);
+  },
+
+  getRandomDishFromCourse(courseName) {
+    const dishes = this._courses[courseName];
+    const randomIndex = Math.floor(Math.random() * dishes.length);
+    return dishes[randomIndex];
+  },
+
+  generateRandomMeal() {
+    const appetizer = this.getRandomDishFromCourse("appetizers");
+    const main = this.getRandomDishFromCourse("mains");
+    const dessert = this.getRandomDishFromCourse("desserts");
+    const totalPrice = appetizer.dishPrice + main.dishPrice + dessert.dishPrice;
+    return `Total price for ${appetizer.dishName}, ${main.dishName}, ${dessert.dishName} is: ${totalPrice}`;
+  },
 };
 
-console.log(menu.courses);
+menu.addDishToCourse("appetizers", "cezar salad", 6.5);
+menu.addDishToCourse("appetizers", "tapa", 4.5);
+console.log(menu.appetizers);
+
+menu.addDishToCourse("desserts", "brownie", 2.9);
+console.log(menu.desserts);
+
+menu.addDishToCourse("mains", "meat and potatos", 9.5);
+console.log(menu.mains);
+
+console.log(menu.generateRandomMeal());
